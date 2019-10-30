6486bae8f7ee838513a95ec603778a5d02ef10b4
Luis Marchani <luis.r.marchani.-nd@disney.com>
2019-10-30 13:59:33 -0500

    newnew

---
+++ BaseTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestsAzure
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        public IWebDriver driver;
+        public TestContext TestContext { get; set; }
+
+        [SetUp]
+        public void setup()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("headless");
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions);
+        }
+            
+
+        [TearDown]
+        public void teardown()
+        {
+            try
+            {
+                if (TestContext.CurrentContext.Result.Outcome.Status != NUnit.Framework.Interfaces.TestStatus.Passed)
+                {
+                    Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+                    string path = Directory.GetCurrentDirectory() + TestContext.CurrentContext.Test.MethodName + DateTime.Now.ToString().Trim().Replace("/","_").Replace(":","_").Replace(" ","_") + ".png";
+                    ss.SaveAsFile(path);
+                    TestContext.AddTestAttachment(path);
+
+
+                }
+            }
+            finally
+            {
+                driver.Close();
+                driver.Quit();
+            }
+            
+
+        }
+    }
+}
---
+++ SeleniumDemo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTestsAzure
+{
+    [TestFixture]
+    public class SeleniumDemo : BaseTest
+    {
+        [Test]
+        public void SearchForCheese()
+        {
+            /*var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("headless");
+            using(var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+            {*/
+                driver.Navigate().GoToUrl("http://www.google.com");
+                IWebElement query = driver.FindElement(By.Name("q"));
+                query.SendKeys("cheese");
+                query.Submit();
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+                Assert.AreEqual(driver.Title, "cheese - Google Search111");
+
+            //}
+        }
+
+        [TestCase("cake", "cake - Google Search")]
+        [TestCase("carrot", "carrot - Google Search111")]
+        public void SearchForCake(string criteria, string result)
+        {
+            /*var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("headless");
+            using(var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+            {*/
+            driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys(criteria);
+            query.Submit();
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.Title.StartsWith(criteria, StringComparison.OrdinalIgnoreCase));
+
+            Assert.AreEqual(driver.Title, result);
+
+            //}
+        }
+    }
+}
